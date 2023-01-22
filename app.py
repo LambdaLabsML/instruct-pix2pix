d27799d40650490db9e42b124ff18f9e23bac847
Patrick von Platen <patrickvonplaten@users.noreply.huggingface.co>
2023-01-22 12:43:09 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
-from diffusers import (
-    StableDiffusionPipeline,
-    StableDiffusionImg2ImgPipeline,
-    DPMSolverMultistepScheduler,
-)
+from diffusers import DiffusionPipeline
 import gradio as gr
 import torch
 from PIL import Image
@@ -14,40 +10,8 @@ from diffusers.pipelines.stable_diffusion.safety_checker import StableDiffusionS
 
 start_time = time.time()
 current_steps = 25
-
-SAFETY_CHECKER = StableDiffusionSafetyChecker.from_pretrained("CompVis/stable-diffusion-safety-checker", torch_dtype=torch.float16)
-
-
-class Model:
-    def __init__(self, name, path=""):
-        self.name = name
-        self.path = path
-
-        if path != "":
-            self.pipe_t2i = StableDiffusionPipeline.from_pretrained(
-                path, torch_dtype=torch.float16, safety_checker=SAFETY_CHECKER
-            )
-            self.pipe_t2i.scheduler = DPMSolverMultistepScheduler.from_config(
-                self.pipe_t2i.scheduler.config
-            )
-            self.pipe_i2i = StableDiffusionImg2ImgPipeline(**self.pipe_t2i.components)
-        else:
-            self.pipe_t2i = None
-            self.pipe_i2i = None
-
-
-models = [
-    Model("Protogen v2.2 (Anime)", "darkstorm2150/Protogen_v2.2_Official_Release"),
-    Model("Protogen x3.4 (Photorealism)", "darkstorm2150/Protogen_x3.4_Official_Release"),
-    Model("Protogen x5.3 (Photorealism)", "darkstorm2150/Protogen_x5.3_Official_Release"),
-    Model("Protogen x5.8 Rebuilt (Scifi+Anime)", "darkstorm2150/Protogen_x5.8_Official_Release"),
-    Model("Protogen Dragon (RPG Model)", "darkstorm2150/Protogen_Dragon_Official_Release"),
-    Model("Protogen Nova", "darkstorm2150/Protogen_Nova_Official_Release"),
-    Model("Protogen Eclipse", "darkstorm2150/Protogen_Eclipse_Official_Release"),
-    Model("Protogen Infinity", "darkstorm2150/Protogen_Infinity_Official_Release"),
-]
-
-MODELS = {m.name: m for m in models}
+        
+PIPE = DiffusionPipeline.from_pretrained("timbrooks/instruct-pix2pix", torch_dtype=torch.float16, safety_checker=None)
 
 device = "GPU 🔥" if torch.cuda.is_available() else "CPU 🥶"
 
@@ -83,77 +47,25 @@ def inference(
     generator = torch.Generator("cuda").manual_seed(seed)
 
     try:
-        if img is not None:
-            return (
-                img_to_img(
-                    model_name,
-                    prompt,
-                    n_images,
-                    neg_prompt,
-                    img,
-                    strength,
-                    guidance,
-                    steps,
-                    width,
-                    height,
-                    generator,
-                    seed,
-                ),
-                f"Done. Seed: {seed}",
-            )
-        else:
-            return (
-                txt_to_img(
-                    model_name,
-                    prompt,
-                    n_images,
-                    neg_prompt,
-                    guidance,
-                    steps,
-                    width,
-                    height,
-                    generator,
-                    seed,
-                ),
-                f"Done. Seed: {seed}",
-            )
+        return (
+            img_to_img(
+                model_name,
+                prompt,
+                n_images,
+                neg_prompt,
+                img,
+                strength,
+                guidance,
+                steps,
+                width,
+                height,
+                generator,
+                seed,
+            ),
+            f"Done. Seed: {seed}",
+        )
     except Exception as e:
         return None, error_str(e)
-
-
-def txt_to_img(
-    model_name,
-    prompt,
-    n_images,
-    neg_prompt,
-    guidance,
-    steps,
-    width,
-    height,
-    generator,
-    seed,
-):
-    pipe = MODELS[model_name].pipe_t2i
-
-    if torch.cuda.is_available():
-        pipe = pipe.to("cuda")
-        pipe.enable_xformers_memory_efficient_attention()
-
-    result = pipe(
-        prompt,
-        negative_prompt=neg_prompt,
-        num_images_per_prompt=n_images,
-        num_inference_steps=int(steps),
-        guidance_scale=guidance,
-        width=width,
-        height=height,
-        generator=generator,
-    )
-
-    pipe.to("cpu")
-    torch.cuda.empty_cache()
-
-    return replace_nsfw_images(result)
 
 
 def img_to_img(
@@ -170,7 +82,7 @@ def img_to_img(
     generator,
     seed,
 ):
-    pipe = MODELS[model_name].pipe_i2i
+    pipe = PIPE
 
     if torch.cuda.is_available():
         pipe = pipe.to("cuda")
@@ -190,10 +102,8 @@ def img_to_img(
         generator=generator,
     )
 
-    pipe.to("cpu")
-    torch.cuda.empty_cache()
-
-    return replace_nsfw_images(result)
+    # return replace_nsfw_images(result)
+    return result.images
 
 
 def replace_nsfw_images(results):
@@ -222,7 +132,6 @@ with gr.Blocks(css="style.css") as demo:
         """
     )
     with gr.Row():
-
         with gr.Column(scale=55):
             with gr.Group():
                 model_name = gr.Dropdown(
@@ -231,11 +140,6 @@ with gr.Blocks(css="style.css") as demo:
                     value=models[0].name,
                 )
                 with gr.Box(visible=False) as custom_model_group:
-                    custom_model_path = gr.Textbox(
-                        label="Custom model path",
-                        placeholder="Path to model, e.g. darkstorm2150/Protogen_x3.4_Official_Release",
-                        interactive=True,
-                    )
                     gr.HTML(
                         "<div><font size='2'>Custom models have to be downloaded first, so give it some time.</font></div>"
                     )
@@ -285,19 +189,10 @@ with gr.Blocks(css="style.css") as demo:
                             step=1,
                         )
 
-                    with gr.Row():
-                        width = gr.Slider(
-                            label="Width", value=512, minimum=64, maximum=1024, step=8
-                        )
-                        height = gr.Slider(
-                            label="Height", value=512, minimum=64, maximum=1024, step=8
-                        )
-
                     seed = gr.Slider(
                         0, 2147483647, label="Seed (0 = random)", value=0, step=1
                     )
 
-            with gr.Tab("Image to image"):
                 with gr.Group():
                     image = gr.Image(
                         label="Image", height=256, tool="editor", type="pil"
@@ -328,20 +223,11 @@ with gr.Blocks(css="style.css") as demo:
     generate.click(inference, inputs=inputs, outputs=outputs)
 
     ex = gr.Examples(
-        [
-            [models[0].name, "portrait of a beautiful alyx vance half life", 10, 50, "canvas frame, ((disfigured)), ((bad art)), ((deformed)),((extra limbs)),((close up)),((b&w)), weird colors, blurry, (((duplicate))), ((morbid)), ((mutilated)), [out of frame], extra fingers, mutated hands, ((poorly drawn hands)), ((poorly drawn face)), (((mutation))), (((deformed))), blurry, ((bad anatomy)), (((bad proportions))), ((extra limbs)), cloned face, (((disfigured))), out of frame, ugly, extra limbs, (bad anatomy), gross proportions, (malformed limbs), ((missing arms)), ((missing legs)), (((extra arms))), (((extra legs))), mutated hands, (fused fingers), (too many fingers), (((long neck))), Photoshop, video game, ugly, tiling, poorly drawn hands, poorly drawn feet, poorly drawn face, out of frame, mutation, mutated, extra limbs, extra legs, extra arms, disfigured, deformed, cross-eye, body out of frame, blurry, bad art, bad anatomy"],
-            [models[1].name, "Brad Pitt with sunglasses, highly realistic", 7.5, 50, "canvas frame, ((disfigured)), ((bad art)), ((deformed)),((extra limbs)),((close up)),((b&w)), weird colors, blurry, (((duplicate))), ((morbid)), ((mutilated)), [out of frame], extra fingers, mutated hands, ((poorly drawn hands)), ((poorly drawn face)), (((mutation))), (((deformed))), blurry, ((bad anatomy)), (((bad proportions))), ((extra limbs)), cloned face, (((disfigured))), out of frame, ugly, extra limbs, (bad anatomy), gross proportions, (malformed limbs), ((missing arms)), ((missing legs)), (((extra arms))), (((extra legs))), mutated hands, (fused fingers), (too many fingers), (((long neck))), Photoshop, video game, ugly, tiling, poorly drawn hands, poorly drawn feet, poorly drawn face, out of frame, mutation, mutated, extra limbs, extra legs, extra arms, disfigured, deformed, cross-eye, body out of frame, blurry, bad art, bad anatomy"],
-            [models[2].name, "(extremely detailed CG unity 8k wallpaper), the most beautiful artwork in the world", 7.5, 50, "human, people, canvas frame, ((disfigured)), ((bad art)), ((deformed)),((extra limbs)),((close up)),((b&w)), weird colors, blurry, (((duplicate))), ((morbid)), ((mutilated)), [out of frame], extra fingers, mutated hands, ((poorly drawn hands)), ((poorly drawn face)), (((mutation))), (((deformed))), blurry, ((bad anatomy)), (((bad proportions))), ((extra limbs)), cloned face, (((disfigured))), out of frame, ugly, extra limbs, (bad anatomy), gross proportions, (malformed limbs), ((missing arms)), ((missing legs)), (((extra arms))), (((extra legs))), mutated hands, (fused fingers), (too many fingers), (((long neck))), Photoshop, video game, ugly, tiling, poorly drawn hands, poorly drawn feet, poorly drawn face, out of frame, mutation, mutated, extra limbs, extra legs, extra arms, disfigured, deformed, cross-eye, body out of frame, blurry, bad art, bad anatomy"],
-            [models[3].name, "(extremely detailed CG unity 8k wallpaper), full shot body photo star lord chris pratt posing in an outdoor spaceship, holding a gun, extremely detailed, trending on ArtStation, trending on CGSociety, Intricate, High Detail, dramatic, realism, beautiful and detailed lighting, shadows", 7.5, 50, "canvas frame, ((disfigured)), ((bad art)), ((deformed)),((extra limbs)),((close up)),((b&w)), weird colors, blurry, (((duplicate))), ((morbid)), ((mutilated)), [out of frame], extra fingers, mutated hands, ((poorly drawn hands)), ((poorly drawn face)), (((mutation))), (((deformed))), blurry, ((bad anatomy)), (((bad proportions))), ((extra limbs)), cloned face, (((disfigured))), out of frame, ugly, extra limbs, (bad anatomy), gross proportions, (malformed limbs), ((missing arms)), ((missing legs)), (((extra arms))), (((extra legs))), mutated hands, (fused fingers), (too many fingers), (((long neck))), Photoshop, video game, ugly, tiling, poorly drawn hands, poorly drawn feet, poorly drawn face, out of frame, mutation, mutated, extra limbs, extra legs, extra arms, disfigured, deformed, cross-eye, body out of frame, blurry, bad art, bad anatomy"],
-            [models[4].name, "(extremely detailed CG unity 8k wallpaper), full body portrait of (david:1.1), staring at us with a mysterious gaze, realistic, masterpiece, highest quality, ((scifi)), lens flare, ((light sparkles)), unreal engine, digital painting, trending on ArtStation, trending on CGSociety, Intricate, High Detail, dramatic, realism, beautiful and detailed lighting, shadows", 7.5, 50, "canvas frame, ((disfigured)), ((bad art)), ((deformed)),((extra limbs)),((close up)),((b&w)), weird colors, blurry, (((duplicate))), ((morbid)), ((mutilated)), [out of frame], extra fingers, mutated hands, ((poorly drawn hands)), ((poorly drawn face)), (((mutation))), (((deformed))), blurry, ((bad anatomy)), (((bad proportions))), ((extra limbs)), cloned face, (((disfigured))), out of frame, ugly, extra limbs, (bad anatomy), gross proportions, (malformed limbs), ((missing arms)), ((missing legs)), (((extra arms))), (((extra legs))), mutated hands, (fused fingers), (too many fingers), (((long neck))), Photoshop, video game, ugly, tiling, poorly drawn hands, poorly drawn feet, poorly drawn face, out of frame, mutation, mutated, extra limbs, extra legs, extra arms, disfigured, deformed, cross-eye, body out of frame, blurry, bad art, bad anatomy"],
-            [models[5].name, "(extremely detailed CG unity 8k wallpaper), full body portrait of (david:1.1), staring at us with a mysterious gaze, realistic, masterpiece, highest quality, ((scifi)), lens flare, ((light sparkles)), unreal engine, digital painting, trending on ArtStation, trending on CGSociety, Intricate, High Detail, dramatic, realism, beautiful and detailed lighting, shadows", 7.5, 50, "canvas frame, ((disfigured)), ((bad art)), ((deformed)),((extra limbs)),((close up)),((b&w)), weird colors, blurry, (((duplicate))), ((morbid)), ((mutilated)), [out of frame], extra fingers, mutated hands, ((poorly drawn hands)), ((poorly drawn face)), (((mutation))), (((deformed))), blurry, ((bad anatomy)), (((bad proportions))), ((extra limbs)), cloned face, (((disfigured))), out of frame, ugly, extra limbs, (bad anatomy), gross proportions, (malformed limbs), ((missing arms)), ((missing legs)), (((extra arms))), (((extra legs))), mutated hands, (fused fingers), (too many fingers), (((long neck))), Photoshop, video game, ugly, tiling, poorly drawn hands, poorly drawn feet, poorly drawn face, out of frame, mutation, mutated, extra limbs, extra legs, extra arms, disfigured, deformed, cross-eye, body out of frame, blurry, bad art, bad anatomy"],
-            [models[6].name, "(extremely detailed CG unity 8k wallpaper), full body portrait of (david:1.1), staring at us with a mysterious gaze, realistic, masterpiece, highest quality, ((scifi)), lens flare, ((light sparkles)), unreal engine, digital painting, trending on ArtStation, trending on CGSociety, Intricate, High Detail, dramatic, realism, beautiful and detailed lighting, shadows", 7.5, 50, "canvas frame, ((disfigured)), ((bad art)), ((deformed)),((extra limbs)),((close up)),((b&w)), weird colors, blurry, (((duplicate))), ((morbid)), ((mutilated)), [out of frame], extra fingers, mutated hands, ((poorly drawn hands)), ((poorly drawn face)), (((mutation))), (((deformed))), blurry, ((bad anatomy)), (((bad proportions))), ((extra limbs)), cloned face, (((disfigured))), out of frame, ugly, extra limbs, (bad anatomy), gross proportions, (malformed limbs), ((missing arms)), ((missing legs)), (((extra arms))), (((extra legs))), mutated hands, (fused fingers), (too many fingers), (((long neck))), Photoshop, video game, ugly, tiling, poorly drawn hands, poorly drawn feet, poorly drawn face, out of frame, mutation, mutated, extra limbs, extra legs, extra arms, disfigured, deformed, cross-eye, body out of frame, blurry, bad art, bad anatomy"],
-            [models[7].name, "(extremely detailed CG unity 8k wallpaper), full body portrait of (david:1.1), staring at us with a mysterious gaze, realistic, masterpiece, highest quality, ((scifi)), lens flare, ((light sparkles)), unreal engine, digital painting, trending on ArtStation, trending on CGSociety, Intricate, High Detail, dramatic, realism, beautiful and detailed lighting, shadows", 7.5, 50, "canvas frame, ((disfigured)), ((bad art)), ((deformed)),((extra limbs)),((close up)),((b&w)), weird colors, blurry, (((duplicate))), ((morbid)), ((mutilated)), [out of frame], extra fingers, mutated hands, ((poorly drawn hands)), ((poorly drawn face)), (((mutation))), (((deformed))), blurry, ((bad anatomy)), (((bad proportions))), ((extra limbs)), cloned face, (((disfigured))), out of frame, ugly, extra limbs, (bad anatomy), gross proportions, (malformed limbs), ((missing arms)), ((missing legs)), (((extra arms))), (((extra legs))), mutated hands, (fused fingers), (too many fingers), (((long neck))), Photoshop, video game, ugly, tiling, poorly drawn hands, poorly drawn feet, poorly drawn face, out of frame, mutation, mutated, extra limbs, extra legs, extra arms, disfigured, deformed, cross-eye, body out of frame, blurry, bad art, bad anatomy"],
-        ],
+        [],
         inputs=[model_name, prompt, guidance, steps, neg_prompt],
         outputs=outputs,
         fn=inference,
-        cache_examples=False,
+        cache_examples=True,
     )
 
     gr.HTML(
